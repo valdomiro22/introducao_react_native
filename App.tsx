ba9973f1a22d4675a364a2778014a58783bd1a82
Valdomiro Santos <valdomiro.jose@outlook.com>
2024-02-11 03:09:07 -0300

    styles separados mas no mesmo arquivo

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+
 
 export default function App() {
   return(
-    <View style={{
-      flex: 1,
-      backgroundColor: '#131016',
-      padding: 24
-    }}>
-      <Text style={{
-        color: '#fdfcfe',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 48
-      }}>Nome do evento</Text>
-      
-      <Text style={{color: '#6b6b6b', fontSize: 18}}>Sexta, 4 de Novembro de 2022</Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Domingo, 4 de Novembro de 2022</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+  },
+  eventName: {
+    color: '#fdfcfe',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: '#aaa',
+    fontSize: 14
+  }
+});
