e0284484fb42f7904c74d9866d7e7ce7c1470695
Valdomiro Santos <valdomiro.jose@outlook.com>
2024-02-11 08:10:13 -0300

    FlatList x ScrowView

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, TextInput, TouchableOpacity, ScrollView } from "react-native";
+import { Text, View, TextInput, TouchableOpacity, FlatList } from "react-native";
 import { styles } from "./styles";
 import { Participant } from "../../components/Participant";
 
@@ -31,17 +31,23 @@ export function Home() {
         </TouchableOpacity>
       </View>
 
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {
-          participants.map(participant => (
-            <Participant 
-            key={participant}
-            name={participant} 
-            onRemove={ () => handleParticipantRemove("Valdomiro") }/>
-            ))
-        }
-      </ScrollView>
-
+      <FlatList 
+        data={participants}
+        // data={[]}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Participant 
+          key={item}
+          name={item} 
+          onRemove={ () => handleParticipantRemove("Valdomiro") }/>
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participantes à sua lista de presença.
+          </Text>
+        )}
+      />
     </View>
   )
 }
